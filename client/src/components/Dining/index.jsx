ecdfe1bf4a322e822548aad9f80da4c7cb60f8c2
sourav0kumar <souravgupta1401@gmail.com>
2022-12-18 16:41:42 +0530

    Completed restaurant cards and completed dining page

---
+++ client/src/components/Dining/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Dining = () => {
+  return <div>Dining</div>;
+};
+
+export default Dining;
+import React from "react";
+
+// components
+import DiningCarousel from "./DiningCarousel";
+
+const Dining = () => {
+  return (
+    <div className="mb-10">
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Collections
+      </h1>
+      <DiningCarousel />
+    </div>
+  );
+};
+
+export default Dining;
--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from "react-router-dom";
+
 import HomepageLayout from '../layouts/Homepage.layout';
 
+// components
+import Delivery from "../components/Delivery";
+import Dining from "../components/Dining";
+import NightLife from "../components/NightLife";
+import Nutrition from "../components/Nutrition";
 const Home = () => {
+  const { type } = useParams();
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="my-5">
+        {type === "delivery" && <Delivery />}
+        {type === "dining" && <Dining />}
+        {type === "night" && <NightLife />}
+        {type === "nutri" && <Nutrition />}
+      </div>
+    </>
+  );
+};
 
 export default HomepageLayout(Home) ;
